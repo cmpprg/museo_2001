87ecf2959a9ec660c3a6470a45a6b9861ec16240
Ryan Camp <cmpprg@icloud.com>
2020-03-04 09:10:27 -0700

    Create attributes for Photograph class

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,5 +1,10 @@
 class Photograph
-  def initialize(argument)
-    @argument = argument
+
+  attr_reader :name, :id, :artist_id, :year
+  def initialize(photo_info)
+    @id = photo_info[:id]
+    @name = photo_info[:name]
+    @artist_id = photo_info[:artist_id]
+    @year = photo_info[:year]
   end
 end
