6f8eba9b8d464d7507ee3989446fd5c709d8b55a
Ryan Camp <cmpprg@icloud.com>
2020-03-04 09:27:15 -0700

    Create #add_photograph for Curator class

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -7,6 +7,19 @@ require "./lib/artist"
 class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
   end
 
   def test_it_exists
@@ -15,5 +28,12 @@ class CuratorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal [], @curator.photographs
+  end
+
+  def test_it_can_add_a_photograph
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
   end
 end
