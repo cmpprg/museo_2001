85145d32dba4b0eb3148a5cd749162e6a3235d21
Ryan Camp <cmpprg@icloud.com>
2020-03-04 09:31:46 -0700

    Create #add_artist for Curator class

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,22 @@ class CuratorTest < Minitest::Test
          artist_id: "2",
          year: "1941"
     })
+
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
@@ -28,6 +44,7 @@ class CuratorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
   def test_it_can_add_a_photograph
@@ -35,5 +52,12 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
 
     assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_can_add_an_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
   end
 end
